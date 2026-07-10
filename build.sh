27c98e60767b82cd17af839f69d7b64475753173
Dominic Kramer <DominicKramer@gmail.com>
2026-07-09 18:16:39 -0700

    do not use a base path

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ cd ../mathlingua
 cargo build --release
 cp target/release/mlg ../mathlore
 cd ../mathlore
-./mlg export --force --output docs --base-path /mathlore/ --cname www.mathlore.org
+./mlg export --force --output docs --cname www.mathlore.org
 rm mlg
