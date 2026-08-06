c5dcf70a4aa703aaa947cb863be0156418419e36
Dominic Kramer <DominicKramer@gmail.com>
2026-08-06 15:36:52 -0700

    snapshot

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ cd ../mathlingua
 cargo build --release
 cp target/release/mlg ../mathlore
 cd ../mathlore
-./mlg export --force --output docs --cname www.mathlore.org
+./mlg export --force --cname www.mathlore.org
 rm mlg
